6a9192b8270c78a2569f44e3717e293bb4547ea2
Crowdbotics <team@crowdbotics.com>
2023-08-11 05:23:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "a-time-to-remember-42785",
+  url: "https://a-time-to-remember-42785.botics.co",
+  api: "https://a-time-to-remember-42785.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
